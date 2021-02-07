849a511bdc4e82b292cd3776d660640f3a335766
dhavalmaniyar <logoutdhaval@gmail.com>
2021-02-07 18:06:15 +0530

    new script

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,10 @@ drone.on('open', error => {
   // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
     console.log('MEMBERS', members);
+    console.log('>>>>>>>>>>>>>>>>>');
+    if (members.length>=1){
+      return alert('The room is full');
+    }
     // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
